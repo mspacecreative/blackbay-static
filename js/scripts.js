b141a8db38d6ebe5d9162f3b3379a2dc82fa4cea
Matt <matt@mspacecreative.com>
2018-01-12 08:13:17 -0400

    c

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,5 @@
 // HOVER FUNCTIONALITY ON MOBILE
-$('a.taphover').on('touchstart', function (e) {
-    'use strict'; //satisfy code inspectors
-    var link = $(this); //preselect the link
-    if (link.hasClass('hover')) {
-        return true;
-    } else {
-        link.addClass('hover');
-        $('a.taphover').not(this).removeClass('hover');
-        e.preventDefault();
-        return false; //extra, and to make sure the function has consistent return points
-    }
-});
+$('.taphover').bind('touchstart', function() {});
 
 // MOBILE NAVIGATION
 $('.mobile-nav li.menu-item-has-children').prepend('<span class="sub-toggle"><i class="fa fa-angle-down"></i></span>');
