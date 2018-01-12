ae8a0409553fc5196f2566994a11dacdbdcdbe1d
Matt <matt@mspacecreative.com>
2018-01-12 08:09:18 -0400

    c

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 // HOVER FUNCTIONALITY ON MOBILE
-$('.hover').bind('touchstart touchend', function(e) {
-  e.preventDefault();
-  $(this).toggleClass('hover_effect');
+$('a.taphover').on('touchstart', function (e) {
+    'use strict'; //satisfy code inspectors
+    var link = $(this); //preselect the link
+    if (link.hasClass('hover')) {
+        return true;
+    } else {
+        link.addClass('hover');
+        $('a.taphover').not(this).removeClass('hover');
+        e.preventDefault();
+        return false; //extra, and to make sure the function has consistent return points
+    }
 });
 
 // MOBILE NAVIGATION
