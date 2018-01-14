f410a244ea48c12cd519cbc672306300439aef41
Matt <matt@mspacecreative.com>
2018-01-14 18:22:12 -0400

    c

--- js/scripts.js
+++ js/scripts.js
@@ -120,6 +120,7 @@ $(document).ready(function () {
        slidesToShow: 1,
        slidesToScroll: 1,
        arrows: false,
+       adaptiveHeight: true,
        asNavFor: '.timeline'
      });
      $('.timeline').slick({
