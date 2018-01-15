d3058b017e0786019cb72e2dde25d507f736146c
Matt <matt@mspacecreative.com>
2018-01-15 10:19:58 -0400

    c

--- js/scripts.js
+++ js/scripts.js
@@ -135,7 +135,32 @@ $(document).ready(function () {
        asNavFor: '.slider-for,.slick-bg',
        centerMode: true,
        focusOnSelect: true,
-       infinite: true
+       infinite: true,
+       responsive: [
+           {
+             breakpoint: 1024,
+             settings: {
+               slidesToShow: 3,
+               slidesToScroll: 3,
+               infinite: true,
+               dots: true
+             }
+           },
+           {
+             breakpoint: 600,
+             settings: {
+               slidesToShow: 2,
+               slidesToScroll: 2
+             }
+           },
+           {
+             breakpoint: 480,
+             settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1
+             }
+           }
+         ]
      });
      $('.hide-onload').css('visibility', 'hidden');
      $('.slick-next, .slick-slide').click(function () {
