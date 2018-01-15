e7a06183c706fa993cfde5ed84fa2dc67b9cdd95
Matt <matt@mspacecreative.com>
2018-01-15 10:30:47 -0400

    c

--- js/scripts.js
+++ js/scripts.js
@@ -147,10 +147,10 @@ $(document).ready(function () {
              }
            },
            {
-             breakpoint: 600,
+             breakpoint: 480,
              settings: {
-               slidesToShow: 2,
-               slidesToScroll: 2,
+               slidesToShow: 1,
+               slidesToScroll: 1,
                centerPadding: '5%',
              }
            }
